85b1f5b05fb5642e9b69373291148914b326ff67
CharlesGe129 <char1209ge@gmail.com>
2019-02-10 19:14:15 -0800

    Test cases

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,406 @@
 include!("merkle_patricia_trie.rs");
 
+use std::fmt;
+use std::io;
+use std::io::{Read, Write};
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[test]
+fn test_mpt_ext() {
+    let mut mpt = MerklePatriciaTrie::new();
+    let mut inserted_trie = String::new();
+    let mut v = String::new();
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("p", "apple");
+    mpt.insert("aa", "banana");
+    mpt.insert("ap", "orange");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("b", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_011.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_011.txt"));
+    mpt.delete("b");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+    v = mpt.get("aa");
+    assert_eq!(v, "banana");
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("p", "apple");
+    mpt.insert("aa", "banana");
+    mpt.insert("ap", "orange");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("ba", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_013.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_013.txt"));
+    mpt.delete("ba");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+    v = mpt.get("p");
+    assert_eq!(v, "apple");
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("aaa", "apple");
+    mpt.insert("aap", "banana");
+    mpt.insert("bb", "right leaf");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("aa", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_030.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_030.txt"));
+    mpt.delete("aa");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("p", "apple");
+    mpt.insert("aaa", "banana");
+    mpt.insert("aap", "orange");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("b", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_031.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_031.txt"));
+    mpt.delete("b");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("p", "apple");
+    mpt.insert("aaa", "banana");
+    mpt.insert("aap", "orange");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("ba", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_033.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_033.txt"));
+    mpt.delete("ba");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("aa", "apple");
+    mpt.insert("ap", "banana");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("b", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_111.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_111.txt"));
+    mpt.delete("b");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("aa", "apple");
+    mpt.insert("ap", "banana");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("bc", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_113.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_113.txt"));
+    mpt.delete("bc");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("p", "apple");
+    mpt.insert("aaaa", "banana");
+    mpt.insert("aaap", "orange");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("a", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_140.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_140.txt"));
+    mpt.delete("a");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("aaa", "apple");
+    mpt.insert("aap", "banana");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("b", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_131.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_131.txt"));
+    mpt.delete("b");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("aaa", "apple");
+    mpt.insert("aap", "banana");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("bc", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_133.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_133.txt"));
+    mpt.delete("bc");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+}
+
+#[test]
+fn test_mpt_leaf() {
+    let mut inserted_trie = String::new();
+    let mut mpt = MerklePatriciaTrie::new();
+    mpt.insert("a", "apple");
+    mpt.insert("b", "banana");
+    mpt.insert("a", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_000.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_000.txt"));
+    mpt.delete("a");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/delete_basic_1.txt"));
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("a", "apple");
+    mpt.insert("b", "banana");
+    mpt.insert("ab", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_002.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_002.txt"));
+    mpt.delete("ab");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/basic_0.txt"));
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("a", "apple");
+    mpt.insert("p", "banana");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("b", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_011.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_011.txt"));
+    mpt.delete("b");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("a", "apple");
+    mpt.insert("p", "banana");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("bc", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_013.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_013.txt"));
+    mpt.delete("bc");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("bab", "apple");
+    mpt.insert("aa", "banana");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("b", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_040.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_040.txt"));
+    mpt.delete("b");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("aab", "apple");
+    mpt.insert("app", "banana");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("ac", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_031.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_031.txt"));
+    mpt.delete("ac");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("aab", "apple");
+    mpt.insert("app", "banana");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("ace", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_033.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_033.txt"));
+    mpt.delete("ace");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("p", "banana");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("a", "apple");
+    mpt.insert("a", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_100.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_100.txt"));
+    mpt.delete("a");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("a", "apple");
+    mpt.insert("p", "banana");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("abc", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_104.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_104.txt"));
+    mpt.delete("abc");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("a", "apple");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("b", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_111.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_111.txt"));
+    mpt.delete("b");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("a", "apple");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("bc", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_113.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_113.txt"));
+    mpt.delete("bc");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("ap", "apple");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("b", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_131.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_131.txt"));
+    mpt.delete("b");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("ap", "apple");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("bp", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_133.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/leaf_133.txt"));
+    mpt.delete("bp");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+}
+
+#[test]
+fn test_mpt_delete_basic() {
+    let mut mpt = MerklePatriciaTrie::new();
+    mpt.insert("a", "apple");
+    mpt.insert("b", "banana");
+    mpt.delete("a");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/delete_basic_1.txt"));
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("a", "apple");
+    mpt.insert("b", "banana");
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/delete_basic_0.txt"));
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("aa", "apple");
+    mpt.insert("abb", "banana");
+    mpt.delete("a");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/delete_basic_2.txt"));
+}
+
+#[test]
+fn test_mpt_branch() {
+    let mut mpt = MerklePatriciaTrie::new();
+    let mut inserted_trie = String::new();
+    mpt.insert("aa", "apple");
+    mpt.insert("ap", "banana");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("a", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/branch_nv_np.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/branch_nv_np.txt"));
+    mpt.delete("a");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("a", "old");
+    mpt.insert("aa", "apple");
+    mpt.insert("ap", "banana");
+    mpt.insert("a", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/branch_v_np.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/branch_v_np.txt"));
+    mpt.delete("a");
+    let mut expected_mpt = MerklePatriciaTrie::new();
+    expected_mpt.insert("aa", "apple");
+    expected_mpt.insert("ap", "banana");
+    assert_eq!(mpt.order_nodes(), expected_mpt.order_nodes());
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("a", "apple");
+    mpt.insert("b", "banana");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("c", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/branch_nv_p.txt"));
+    mpt.delete("cc");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/branch_nv_p.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("aa", "apple");
+    mpt.insert("ap", "banana");
+    mpt.insert("a", "old");
+    inserted_trie = mpt.order_nodes();
+    mpt.insert("aA", "new");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/branch_v_p.txt"));
+    mpt.delete("c");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/branch_v_p.txt"));
+    mpt.delete("aA");
+    assert_eq!(mpt.order_nodes(), inserted_trie);
+}
+
+#[test]
+fn test_mpt_leaf_basic() {
+    let mut mpt = MerklePatriciaTrie::new();
+    mpt.insert("a", "apple");
+    mpt.insert("b", "banana");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/basic_0.txt"));
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("a", "apple");
+    mpt.insert("p", "banana");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/basic_1.txt"));
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("a", "apple");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/basic_2.txt"));
+}
+
+#[test]
+fn test_mpt_ext_basic() {
+    let mut mpt = MerklePatriciaTrie::new();
+    mpt.insert("a", "apple");
+    mpt.insert("b", "banana");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_basic_1.txt"));
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("aa", "apple");
+    mpt.insert("ap", "banana");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_basic_2.txt"));
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("aa", "apple");
+    mpt.insert("ab", "banana");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_basic_3.txt"));
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("aaa", "apple");
+    mpt.insert("aap", "banana");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_basic_4.txt"));
+
+    mpt = MerklePatriciaTrie::new();
+    mpt.insert("p", "apple");
+    mpt.insert("aa", "banana");
+    mpt.insert("ap", "orange");
+    assert_eq!(mpt.order_nodes(), read_file("src/mpt_tests/ext_basic_5.txt"));
+}
+
+fn read_file(path: &str) -> String {
+    let mut f = File::open(path).expect("File not found");
+    let mut content = String::new();
+    f.read_to_string(&mut content).expect("read failed");
+    content
 }
--- src/merkle_patricia_trie.rs
+++ src/merkle_patricia_trie.rs
@@ -10,6 +10,43 @@ enum Node {
     Null(),
 }
 
+impl fmt::Debug for Node {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Node::Null() => write!(f, "[Null Node]"),
+            Node::Branch(branch) => {
+                write!(f, "Branch[");
+                for i in 0..16 {
+                    write!(f, "{}=\"{}\", ", i, branch[i]);
+                }
+                write!(f, "value={}]", branch[16])
+            },
+            Node::Flag((encoded_prefix, value)) => {
+                write!(f, "{}<{:?}, value=\"{}\">",
+                       if is_ext_node(encoded_prefix.to_vec()) {"Ext"} else {"Leaf"},
+                       compact_decode(encoded_prefix.to_vec()),
+                       value)
+            }
+        }
+    }
+}
+
+impl Node {
+    fn clone(&self) -> Node {
+        match self {
+            Node::Flag((prefix, value)) => Node::Flag((prefix.to_vec(), value.to_string())),
+            Node::Branch(branch) => {
+                let mut value: [String; 17] = empty_branch_value();
+                for i in 0..17 {
+                    value[i] = branch[i].to_string();
+                }
+                Node::Branch(value)
+            },
+            _ => Node::Null(),
+        }
+    }
+}
+
 #[derive(Debug)]
 struct MerklePatriciaTrie {
     db: HashMap<String, Node>,
@@ -17,6 +54,20 @@ struct MerklePatriciaTrie {
 }
 
 impl MerklePatriciaTrie {
+    fn new() -> MerklePatriciaTrie {
+        let mut mpt = MerklePatriciaTrie {db: HashMap::new(), root: String::new()};
+        mpt.db.insert(String::new(), Node::Null());
+        mpt
+    }
+
+    fn clone(&self) -> MerklePatriciaTrie {
+        let mut hashmap = HashMap::new();
+        for (k, v) in self.db.iter() {
+            hashmap.insert((*k).to_string(), (*v).clone());
+        }
+        MerklePatriciaTrie {db: hashmap, root: self.root.to_string()}
+    }
+
     fn get(&mut self, key: &str) -> String {
         // TODO
     }
@@ -28,6 +79,57 @@ impl MerklePatriciaTrie {
     fn delete(&mut self, key: &str) {
         // TODO
     }
+
+    fn mpt_to_string(&self) -> String {
+        let mut content: String = String::new();
+        content = content + &*format!("ROOT={}\n", self.root);
+        for (hash, node) in &self.db {
+            content = content + &*format!("{}: {:?}\n", hash, node);
+        }
+        content
+    }
+
+    fn print(&self) {
+        println!("{}", self.mpt_to_string());
+    }
+
+    fn order_nodes(&self) -> String {
+        let raw_content = self.mpt_to_string();
+        let content: Vec<&str> = raw_content.split("\n").collect();
+        let mut queue: Vec<&str> = Vec::new();
+        let mut temp1 = content[0];
+        let mut temp2: Vec<&str> = temp1.split("HashStart").collect();
+        temp1 = temp2[1];
+        temp2 = temp1.split("HashEnd").collect();
+        temp1 = temp2[0];
+        queue.push(temp1);
+        let mut i = -1;
+        let mut rs = String::new();
+        while let Some(cur_hash) = queue.pop() {
+            i += 1;
+            println!("cur={}", cur_hash);
+            let mut line: &str = "";
+            for each in &content {
+                if each.starts_with(&*(format!("HashStart{}HashEnd", cur_hash))) {
+                    temp2 = each.split("HashEnd: ").collect();
+                    line = temp2[1];
+                    rs = rs + each + "\n";
+                    rs = rs.replace(&*(format!("HashStart{}HashEnd", cur_hash)), &*(format!("Hash{}", i)));
+                }
+            }
+            temp2 = line.split("HashStart").collect();
+            let mut flag = true;
+            for each in temp2 {
+                if flag {
+                    flag = false;
+                    continue
+                }
+                let temp3: Vec<&str> = each.split("HashEnd").collect();
+                queue.push(temp3[0]);
+            }
+        }
+        rs
+    }
 }
 
 fn compact_encode(hex_array: Vec<u8>) -> Vec<u8> {
@@ -37,6 +139,10 @@ fn compact_encode(hex_array: Vec<u8>) -> Vec<u8> {
 // If Leaf, ignore 16 at the end
 fn compact_decode(encoded_arr: Vec<u8>) -> Vec<u8> {
     // TODO
+}
+
+fn is_ext_node(encoded_arr: Vec<u8>) -> bool {
+    (encoded_arr[0] as i32) / 16 < 2
 }
 
 fn test_compact_encode() {
@@ -64,4 +170,10 @@ fn hash_node(node: &Node) -> String {
         Node::Null() => {hasher.input_str("");},
     }
     String::from("HashStart_") + &*(hasher.result_str()) + "_HashEnd"
+}
+
+fn empty_branch_value() -> [String; 17] {
+    [String::new(), String::new(), String::new(), String::new(), String::new(), String::new(),
+        String::new(), String::new(), String::new(), String::new(), String::new(), String::new(),
+        String::new(), String::new(), String::new(), String::new(), String::new()]
 }
